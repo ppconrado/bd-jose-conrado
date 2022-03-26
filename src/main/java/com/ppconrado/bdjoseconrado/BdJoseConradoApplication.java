1fc1486cac81e82e53c10c2c93466c73931110e5
ppconrado <ppconrado@yahoo.com.br>
2022-03-26 20:58:16 -0300

    Projeto criado

---
+++ src/main/java/com/ppconrado/bdjoseconrado/BdJoseConradoApplication.java
@@ -0,0 +1,13 @@
+package com.ppconrado.bdjoseconrado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BdJoseConradoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BdJoseConradoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ppconrado/bdjoseconrado/BdJoseConradoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ppconrado.bdjoseconrado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BdJoseConradoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
