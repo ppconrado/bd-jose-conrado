b137727b86abbcf9726e90eeefdada0db10a3be6
ppconrado <ppconrado@yahoo.com.br>
2022-03-27 04:59:20 -0300

    Criando operacao de instanciacao

--- src/main/java/com/ppconrado/bdjoseconrado/BdJoseConradoApplication.java
+++ src/main/java/com/ppconrado/bdjoseconrado/BdJoseConradoApplication.java
@@ -1,13 +1,36 @@
 package com.ppconrado.bdjoseconrado;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.ppconrado.bdjoseconrado.domain.Categoria;
+import com.ppconrado.bdjoseconrado.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class BdJoseConradoApplication {
+public class BdJoseConradoApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BdJoseConradoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+        // ComandLineRunner vai executar esta rotina no inicio do programa		
+		
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Escritorio");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
+		
 	}
 
 }
