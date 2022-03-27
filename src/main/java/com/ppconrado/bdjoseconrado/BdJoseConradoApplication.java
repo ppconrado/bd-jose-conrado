b188dc564d272f21e72ac919fe8a4225613ba0f6
ppconrado <ppconrado@yahoo.com.br>
2022-03-27 20:59:39 -0300

    Relacionamento Estado e Cidade

--- src/main/java/com/ppconrado/bdjoseconrado/BdJoseConradoApplication.java
+++ src/main/java/com/ppconrado/bdjoseconrado/BdJoseConradoApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.ppconrado.bdjoseconrado.domain.Categoria;
+import com.ppconrado.bdjoseconrado.domain.Cidade;
+import com.ppconrado.bdjoseconrado.domain.Estado;
 import com.ppconrado.bdjoseconrado.domain.Produto;
 import com.ppconrado.bdjoseconrado.repositories.CategoriaRepository;
+import com.ppconrado.bdjoseconrado.repositories.CidadeRepository;
+import com.ppconrado.bdjoseconrado.repositories.EstadoRepository;
 import com.ppconrado.bdjoseconrado.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -20,6 +24,12 @@ public class BdJoseConradoApplication implements CommandLineRunner {
 	
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	
+	@Autowired
+	private EstadoRepository estadoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BdJoseConradoApplication.class, args);
@@ -29,6 +39,9 @@ public class BdJoseConradoApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
         // ComandLineRunner vai executar esta rotina no inicio do programa		
+		
+		
+		/////////////////////// Relacao Categoria-Produto /////////////////////
 		
 		Categoria cat1 = new Categoria(null, "Informatica");
 		Categoria cat2 = new Categoria(null, "Escritorio");
@@ -44,12 +57,25 @@ public class BdJoseConradoApplication implements CommandLineRunner {
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 		
-		
+		// Salva no BD
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
-		
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 		
+		//////////////////////// Relacao Estado-Cidade ////////////////////////
 		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "Sao Paulo");
+		
+		Cidade c1 = new Cidade(null, "Uberlandia", est1);
+		Cidade c2 = new Cidade(null, "Sao Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+		
+		// Salva no BD atraves da camada repository (salva estado primeiro)
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 	}
 
 }
